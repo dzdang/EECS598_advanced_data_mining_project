c799fdce81f5bc5ce6871136c9267d7544e32d2b
David Dang <dzdang@umich.edu>
2019-03-14 11:35:13 -0400

    parser working for finding year in given patent file

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char const *argv[])
    if(argc != 3)
    {
       std::cerr << "Expected 3 argument arguments: received: " << argc << std::endl;
+      std::cerr << "Provide arguments in following order: " << std::endl;
       exit(EXIT_FAILURE);
    }
 
@@ -27,13 +28,31 @@ int main(int argc, char const *argv[])
       exit(EXIT_FAILURE);
    }
 
-   // std::string u, v;    //edge defined as (u,v), i.e., u cites v
    int u, v;    //edge defined as (u,v), i.e., u cites v
-   inFile.ignore(1000,'\n');
+   std::string str;
+   std::string year = "1964";
+   std::string node_id = "-1";
 
-   while(inFile >> u >> v)
+   while(!inFile.eof() && node_id == "-1")
    {
-      std::cout << u << v << std::endl;
+      std::getline(inFile, str, '\n');
+
+      if(!str.find("\"")) continue;     //skips the header lines begining with a quote, modify later
+
+      auto pos = str.find(',');
+
+      if(!str.substr(pos + 1).find(year))   //find node_id corresponding to provided year
+      {
+         node_id = str.substr(0,pos);
+         std::cout << node_id << std::endl;
+      }
+   }
+   inFile.close();
+
+   if(node_id == "-1")
+   {
+      std::cout << "Year not found!" << std::endl;
+      exit(EXIT_FAILURE);
    }
 
    return 0;
